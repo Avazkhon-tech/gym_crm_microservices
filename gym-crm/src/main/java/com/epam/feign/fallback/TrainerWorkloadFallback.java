e5ab00549bba8b045023f36f4c55712b5aa23b0b
Avazkhon <avazkhonnazirov33@gmail.com>
2025-06-12 00:24:55 +0500

    implemented fallback

--- gym-crm/src/main/java/com/epam/feign/fallback/TrainerWorkloadFallback.java
+++ gym-crm/src/main/java/com/epam/feign/fallback/TrainerWorkloadFallback.java
@@ -3,12 +3,14 @@ package com.epam.feign.fallback;
 import com.epam.dto.tranining.TrainerWorkloadDto;
 import com.epam.feign.TrainerWorkloadClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class TrainerWorkloadFallback implements TrainerWorkloadClient {
 
     @Override
     public void updateTrainerWorkload(TrainerWorkloadDto trainerWorkloadDto, String token, String transactionId) {
-        log.debug("Circuit breaker triggered! Fallback logic executed.");
+        log.warn("Trainer workload service is down. Update trainer workload is not performed: {}", trainerWorkloadDto);
     }
 }
