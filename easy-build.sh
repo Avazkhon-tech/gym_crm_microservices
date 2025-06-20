96a082e7f1e9ebc7b68272b97366c42f63af5ea2
Avazkhon <avazkhonnazirov33@gmail.com>
2025-06-20 15:16:03 +0500

    improved the custom build script

--- easy-build.sh
+++ easy-build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Cleaning and building eureka-server..."
-cd eureka-server && ./gradlew clean build && cd ..
-
 echo "Cleaning and building gym-crm..."
 cd gym-crm && ./gradlew clean build && cd ..
 
